c5303c0c80ce1613ec4673eae4a32ea09772d597
Javier F. Escribano <javier@touristeye.com>
2013-04-27 11:08:15 +0200

    Remove accents when doing a search in Searchify

--- Service/SearchifyService.php
+++ Service/SearchifyService.php
@@ -252,7 +252,7 @@ class SearchifyService
     public function getResults() {
 
         // Create query
-        $query = $this->term;
+        $query = $this->unaccent($this->term);
 
         // if we want to do a wide search
         if ( $this->autocomplete ) {
@@ -291,6 +291,16 @@ class SearchifyService
         );
 
         return $this->index->search($query, $params);
+    }
+
+    /**
+     * Return an unaccented string
+     * @param  string $string
+     * @return string
+     */
+    private function unaccent($string) {
+        return preg_replace('~&([a-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1',
+            htmlentities($string, ENT_QUOTES, 'UTF-8'));
     }
 
     /**
